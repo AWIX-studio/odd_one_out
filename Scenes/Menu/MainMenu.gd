2a97209777a25d441f6b949cc1476051ce59287d
Slikek <god.boy13243546@gmail.com>
2024-07-22 19:18:49 +0300

    added parallax effect main menu

--- Scenes/Menu/MainMenu.gd
+++ Scenes/Menu/MainMenu.gd
@@ -1,5 +1,15 @@
 extends Control
 
+func set_fg_position(x, y):
+	$FG.position.x = x
+	$FG.position.y = y
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		var new_fg_pos_x = $FG.position[0] + (-get_viewport().size[0]/2 + -event.position[0])
+		var new_fg_pos_y = $FG.position[1] + (-get_viewport().size[1]/2 + -event.position[1])
+		set_fg_position(new_fg_pos_x/60 + 20, new_fg_pos_y/60 + 10)
+
 func _on_play_pressed():
 	get_tree().change_scene_to_file('res://Scenes/Game/Scene1.tscn')
 
