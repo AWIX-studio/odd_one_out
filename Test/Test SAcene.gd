6eba12ff9dbc907ba1a7747a7a58441ae0e579a2
Slikek <god.boy13243546@gmail.com>
2024-07-22 10:12:44 +0300

    Complite first dialogue theme.

--- Test/Test SAcene.gd
+++ Test/Test SAcene.gd
@@ -3,4 +3,8 @@ extends Control
 @onready var dial_box = $DialogueBox
 
 func _on_button_pressed():
-	dial_box.start('Dialogue1')
+	dial_box.start('START')
+
+
+func _on_dialogue_box_option_selected(idx):
+	print($DialogueBox.get_child(1))
