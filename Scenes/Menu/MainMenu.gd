bc11e205ae2de05155df46843c6c8928a65619ef
Slikek <god.boy13243546@gmail.com>
2024-07-23 12:15:56 +0300

    buttons parallax and options menu

--- Scenes/Menu/MainMenu.gd
+++ Scenes/Menu/MainMenu.gd
@@ -3,12 +3,24 @@ extends Control
 func set_fg_position(x, y):
 	$FG.position.x = x
 	$FG.position.y = y
+func set_fg2_position(x, y):
+	$FG2.position.x = x
+	$FG2.position.y = y
+func set_buttons_menu_position(x, y):
+	$Buttons.position.x = x
+	$Buttons.position.y = y
 
 func _input(event):
 	if event is InputEventMouseMotion:
 		var new_fg_pos_x = $FG.position[0] + (-get_viewport().size[0]/2 + -event.position[0])
 		var new_fg_pos_y = $FG.position[1] + (-get_viewport().size[1]/2 + -event.position[1])
-		set_fg_position(new_fg_pos_x/60 + 20, new_fg_pos_y/60 + 10)
+		set_fg_position(new_fg_pos_x/80 + 20, new_fg_pos_y/80)
+		set_fg2_position(new_fg_pos_x/140 + 20, new_fg_pos_y/140)
+		
+		var new_buttons_pos_x = $Buttons.position[0] + (-get_viewport().size[0]/2 + -event.position[0])
+		var new_buttons_pos_y = $Buttons.position[1] + (-get_viewport().size[1]/2 + -event.position[1])
+		set_buttons_menu_position(new_buttons_pos_x/40 + 50, new_buttons_pos_y/40 + 260)
+
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file('res://Scenes/Game/Scene1.tscn')
@@ -16,3 +28,13 @@ func _on_play_pressed():
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_options_pressed():
+	$"Buttons/Main menu".visible = false
+	$Buttons/Options.visible = true
+
+
+func _on_back_pressed():
+	$"Buttons/Main menu".visible = true
+	$Buttons/Options.visible = false
