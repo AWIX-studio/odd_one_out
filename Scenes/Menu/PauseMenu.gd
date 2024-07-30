d07e74c85c710aa7463f5bd02b22e43a1833d2a9
Slikek <god.boy13243546@gmail.com>
2024-07-30 11:37:46 +0300

    pause menu "resume" animation

--- Scenes/Menu/PauseMenu.gd
+++ Scenes/Menu/PauseMenu.gd
@@ -32,7 +32,7 @@ func _on_timer_timeout():
 	is_animation_playing = false
 
 func _on_resume_pressed():
-	hide()
+	pause_hide()
 
 func _on_exit_pressed():
 	get_tree().change_scene_to_file('res://Scenes/Menu/MainMenu.tscn')
