4d1e9d09c9c77653622ec7ea32b50ba698c77ca9
Slikek <god.boy13243546@gmail.com>
2024-07-31 15:46:48 +0300

    the dialog launch system has been rethought

--- Scenes/Game/Scene1.gd
+++ Scenes/Game/Scene1.gd
@@ -1,19 +1,26 @@
 extends Control
 
 var screen_button
-var dialogue_count = 1
+var current_start : String = 'START1'
 
 
 func _ready():
 	$DialogueBox.data = preload("res://Dialogues/chapter 0.tres")
-	$DialogueBox.start('START' + str(dialogue_count))
+	_select_dialogue(current_start)
 
 func _on_dialogue_box_dialogue_ended():
-	if dialogue_count == 1:
-		$BG/NightPark.texture = preload("res://Textures/BGs/school class evening.png")
-	dialogue_count += 1
+	pass
+
+
+func _select_dialogue(_start):
+	current_start = _start
 	$Timer.start(1)
 
 
 func _on_timer_timeout():
-	$DialogueBox.start('START' + str(dialogue_count))
+	$DialogueBox.start(current_start)
+
+
+func _on_dialogue_box_dialogue_signal(_value):
+	if 'START' in _value:
+		_select_dialogue(current_start)
