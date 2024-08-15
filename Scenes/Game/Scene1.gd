6e84954d683f4a6d8f9497b5cd88bb656b21d948
Herowhither <73470226+Herowhither@users.noreply.github.com>
2024-08-15 05:18:33 +0300

    Ну как будта бы +сюжет

--- Scenes/Game/Scene1.gd
+++ Scenes/Game/Scene1.gd
@@ -1,8 +1,11 @@
 extends Control
 
 var screen_button # Это тут было давно, ещё когда я делал активацию диалогов по нажатию на нивидимую кнопку на весь экран. Теперь эта переменная нигде не используется. Используется прямое считывание нажатий мыши. Это я вшил в код плагина (res://addons/dialogue_nodes/objects/DialogueBox.gd : 251 стока).
-var current_start : String = 'START1' # Буффер-переменная чтобы понимать, какой именно диалог запускать. Но лучше запускать всё вручную.
-
+var current_start : String = 'START10' # Буффер-переменная чтобы понимать, какой именно диалог запускать. Но лучше запускать всё вручную.
+var VakumiGood : int
+var VakumiBad : int
+var NakimaGood : int
+var NakimaBad : int
 
 func _ready():
 	$DialogueBox.data = preload("res://Dialogues/chapter 0.tres") # Указывается начальный БГ
@@ -31,6 +34,21 @@ func _on_dialogue_box_dialogue_signal(_value): # _value : String
 	# Тогда можно в Dialogue вызывать сигнал 'mainVacumi1' к примеру и делать проверку:
 	# if 'mainVacumi' in _value:
 	#     _select_dialogue(_value)
-	
+
+	if 'VakumiGood' in _value:
+		VakumiGood += 1
+	if 'VakumiBad' in _value:
+		VakumiBad += 1
+	if 'NakimaGood' in _value:
+		NakimaGood += 1
+	if 'NakimaBad' in _value:
+		NakimaBad += 1
 	if 'START' in _value: # Например 'START1' 'START2' 'START572'. Главное, что есть ключевое слово 'START'. В других проверках лучше не использовать 'START'. Для этого можно создать другую сцену и другой файл диалога.
-		_select_dialogue(current_start)
+		if _value == 'START2':
+			$BG/BGImage.texture = preload('res://Textures/BGs/school class evening.png')
+		if _value == 'START3':
+			$BG/BGImage.texture = preload("res://Textures/BGs/wakumi's house.jpeg")
+		
+		
+		
+		_select_dialogue(_value)
