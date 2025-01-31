f2247f78526557fd0887df79bcb45f1ef07e7baf
AWEDoo <73469414+Av10n@users.noreply.github.com>
2025-01-31 03:45:02 +0300

    imortant commit

--- addons/dialogue_nodes/objects/DialogueBox.gd
+++ addons/dialogue_nodes/objects/DialogueBox.gd
@@ -248,7 +248,7 @@ func _input(event):
 			if _wait_effect:
 				_wait_effect.skip = true
 			_on_wait_finished()
-		
+	
 		# code by awed
 		elif is_running() and Input.is_action_just_pressed(skip_input_action) and options_container.visible:
 			var options_count = 0
